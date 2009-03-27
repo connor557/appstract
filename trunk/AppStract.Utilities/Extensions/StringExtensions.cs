00173b531fcdd56ed881f5bb786c65d2b47a7872
simon_allaeys@hotmail.com <simon_allaeys@hotmail.com@d5324d32-0291-11de-bcc6-d9108be23e13>
2009-03-27 09:14:57 +0000

    Added new extension methods for strings.

--- trunk/AppStract.Utilities/Extensions/StringExtensions.cs
+++ trunk/AppStract.Utilities/Extensions/StringExtensions.cs
@@ -10,20 +10,23 @@ namespace AppStract.Utilities.Extensions
     /// </summary>
     /// <param name="value">This instance.</param>
     /// <param name="values">The strings to compare.</param>
+    /// <returns></returns>
+    public static bool StartsWithAny(this string value, IEnumerable<string> values)
+    {
+      string tmp;
+      return value.StartsWithAny(values, out tmp);
+    }
+
+    /// <summary>
+    /// Determines whether the beginning of this instance matches any of the specified values.
+    /// </summary>
+    /// <param name="value">This instance.</param>
+    /// <param name="values">The strings to compare.</param>
     /// <param name="matchingValue">The matching value, if any.</param>
     /// <returns></returns>
     public static bool StartsWithAny(this string value, IEnumerable<string> values, out string matchingValue)
     {
-      matchingValue = null;
-      foreach (string v in values)
-      {
-        if (value.StartsWith(v))
-        {
-          matchingValue = v;
-          return true;
-        }
-      }
-      return false;
+      return value.StartsWithAny(values, out matchingValue, false);
     }
 
     /// <summary>
@@ -42,6 +45,55 @@ namespace AppStract.Utilities.Extensions
       {
         matchingValue = ignoreCase ? v.ToLowerInvariant() : v;
         if (value.StartsWith(matchingValue))
+        {
+          matchingValue = v;
+          return true;
+        }
+      }
+      matchingValue = null;
+      return false;
+    }
+
+    /// <summary>
+    /// Determines whether the ending of this instance matches any of the specified values.
+    /// </summary>
+    /// <param name="value">This instance.</param>
+    /// <param name="values">The strings to compare.</param>
+    /// <returns></returns>
+    public static bool EndsWithAny(this string value, IEnumerable<string> values)
+    {
+      string tmp;
+      return value.EndsWithAny(values, out tmp);
+    }
+
+    /// <summary>
+    /// Determines whether the ending of this instance matches any of the specified values.
+    /// </summary>
+    /// <param name="value">This instance.</param>
+    /// <param name="values">The strings to compare.</param>
+    /// <param name="matchingValue">The matching value, if any.</param>
+    /// <returns></returns>
+    public static bool EndsWithAny(this string value, IEnumerable<string> values, out string matchingValue)
+    {
+      return value.EndsWithAny(values, out matchingValue, false);
+    }
+
+    /// <summary>
+    /// Determines whether the ending of this instance matches any of the specified values.
+    /// </summary>
+    /// <param name="value">This instance.</param>
+    /// <param name="values">The strings to compare.</param>
+    /// <param name="matchingValue">The matching value, if any.</param>
+    /// <param name="ignoreCase">True to ignore case when comparing; otherwise, false.</param>
+    /// <returns></returns>
+    public static bool EndsWithAny(this string value, IEnumerable<string> values, out string matchingValue, bool ignoreCase)
+    {
+      if (ignoreCase)
+        value = value.ToLowerInvariant();
+      foreach (string v in values)
+      {
+        matchingValue = ignoreCase ? v.ToLowerInvariant() : v;
+        if (value.EndsWith(matchingValue))
         {
           matchingValue = v;
           return true;
