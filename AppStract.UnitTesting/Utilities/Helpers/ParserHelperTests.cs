b5811f39940e99f4ae251e711947162afeafd3db
simon_allaeys <simon_allaeys@hotmail.com>
2010-02-10 11:14:52 +0000

    Unit tests and fixes for AppStract.Utilities.Helpers.ParserHelper, related to normal enums (having no FlagsAttribute)

--- AppStract.UnitTesting/Utilities/Helpers/ParserHelperTests.cs
+++ AppStract.UnitTesting/Utilities/Helpers/ParserHelperTests.cs
@@ -54,17 +54,39 @@ namespace AppStract.UnitTesting.Utilities.Helpers
   public class ParserHelperTests
   {
 
+    [Test]
     public void EnumTest()
     {
-      
+      MyEnum result;
+      Assert.IsTrue(ParserHelper.TryParseEnum(1, out result), "Expected value 1 to be parsable");
+      Assert.IsTrue(result == MyEnum.One, "Expected " + MyEnum.One + " but encountered " + result);
+      Assert.IsTrue(ParserHelper.TryParseEnum("TwO", out result), "Expected string \"Tw0\" to be parsable");
+      Assert.IsTrue(result == MyEnum.Two, "Expected " + MyEnum.Two + " but encountered " + result);
+      Assert.IsTrue(ParserHelper.TryParseEnum((object) 8, out result), "Expected object 8 to be parsable");
+      Assert.IsTrue(result == MyEnum.Eight, "Expected " + MyEnum.Eight + " but encountered " + result);
+      Assert.IsTrue(ParserHelper.TryParseEnum((object)"Eight", out result), "Expected object \"Eight\" to be parsable");
+      Assert.IsTrue(result == MyEnum.Eight, "Expected " + MyEnum.Eight + " but encountered " + result);
+    }
+
+    [Test]
+    public void EnumIllegalTest()
+    {
+      MyEnum result;
+      Assert.IsFalse(ParserHelper.TryParseEnum(3, out result), "Expected value 3 not to be parsable");
+      Assert.IsTrue(result == (default(MyEnum)), "Expected default value " + default(MyEnum) + " but encountered " + result);
+      Assert.IsFalse(ParserHelper.TryParseEnum("IllegalValue", out result), "Expected string \"IllegalValue\" not to be parsable");
+      Assert.IsTrue(result == (default(MyEnum)), "Expected default value " + default(MyEnum) + " but encountered " + result);
+      Assert.IsFalse(ParserHelper.TryParseEnum((object) 9, out result), "Expected object 9 not to be parsable");
+      Assert.IsTrue(result == (default(MyEnum)), "Expected default value " + default(MyEnum) + " but encountered " + result);
+      Assert.IsFalse(ParserHelper.TryParseEnum((object) "IllegalValue", out result), "Expected object \"IllegalValue\" not to be parsable");
+      Assert.IsTrue(result == (default(MyEnum)), "Expected default value " + default(MyEnum) + " but encountered " + result);
     }
 
     [Test]
     public void FlagsIntegerTest()
     {
       MyFlagsEnum result;
-      Assert.IsTrue(ParserHelper.TryParseEnum(15, out result),
-                    "Expected value 15 to be parsable.");
+      Assert.IsTrue(ParserHelper.TryParseEnum(15, out result), "Expected value 15 to be parsable.");
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         Assert.IsTrue(result.IsSpecified(flag), flag + " is not specified in the result.");
     }
@@ -73,8 +95,7 @@ namespace AppStract.UnitTesting.Utilities.Helpers
     public void FlagsIllegalIntegerTest()
     {
       MyFlagsEnum result;
-      Assert.IsFalse(ParserHelper.TryParseEnum(16, out result),
-                     "Expected value 16 not to be parsable.");
+      Assert.IsFalse(ParserHelper.TryParseEnum(16, out result), "Expected value 16 not to be parsable.");
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         if (flag != default(MyFlagsEnum))
           Assert.IsFalse(result.IsSpecified(flag), flag + " is specified in the result.");
@@ -88,8 +109,7 @@ namespace AppStract.UnitTesting.Utilities.Helpers
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         value += flag + ", ";
       value = value.Substring(0, value.Length - 2);
-      Assert.IsTrue(ParserHelper.TryParseEnum(value, out result),
-                    "Expected string to be parsable.");
+      Assert.IsTrue(ParserHelper.TryParseEnum(value, out result), "Expected string to be parsable.");
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         Assert.IsTrue(result.IsSpecified(flag), flag + " is not specified in the result.");
     }
@@ -102,8 +122,7 @@ namespace AppStract.UnitTesting.Utilities.Helpers
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         value += flag + ", ";
       value += "MyIllegalValue";
-      Assert.IsFalse(ParserHelper.TryParseEnum(value, out result),
-                     "Expected string not to be parsable.");
+      Assert.IsFalse(ParserHelper.TryParseEnum(value, out result), "Expected string not to be parsable.");
       foreach (MyFlagsEnum flag in Enum.GetValues(typeof (MyFlagsEnum)))
         if (flag != default(MyFlagsEnum))
           Assert.IsFalse(result.IsSpecified(flag), flag + " is specified in the result.");
