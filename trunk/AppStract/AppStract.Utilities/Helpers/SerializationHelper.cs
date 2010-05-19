57639c4f4c937cc488c8fea9d6a960979b79d5cf
simon_allaeys@hotmail.com <simon_allaeys@hotmail.com@d5324d32-0291-11de-bcc6-d9108be23e13>
2010-05-19 16:47:10 +0000

    Renamed XmlSerializationHelper to SerializationHelper and made this class support both XML and binary de/serialization.

--- trunk/AppStract/AppStract.Utilities/Helpers/SerializationHelper.cs
+++ trunk/AppStract/AppStract.Utilities/Helpers/SerializationHelper.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright (C) 2008-2009 Simon Allaeys
+﻿#region Copyright (C) 2009-2010 Simon Allaeys
 
 /*
     Copyright (C) 2008-2009 Simon Allaeys
@@ -24,16 +24,20 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Xml.Serialization;
 
 namespace AppStract.Utilities.Helpers
 {
+
   /// <summary>
   /// Serializes and deserializes objects into and from XML documents.
   /// </summary>
-  public static class XmlSerializationHelper
+  public static class SerializationHelper
   {
+
+    #region Public Methods
 
     /// <summary>
     /// Deserializes an object from type <typeparamref name="T"/> from the specified file.
@@ -42,66 +46,54 @@ namespace AppStract.Utilities.Helpers
     /// A <see cref="SerializationException"/> is thrown if the content of the file
     /// can't be deserialized to an object of type <see cref="T"/>.
     /// </exception>
-    /// <typeparam name="T">The type of the object to deserialize from the XML-file specified.</typeparam>
+    /// <typeparam name="T">The type of the object to deserialize from the file specified.</typeparam>
     /// <param name="filename">The file containing the XML-data to deserialize.</param>
+    /// <param name="serializerType">The type of serializer to use.</param>
     /// <returns>The deserialized object.</returns>
-    public static T Deserialize<T>(string filename)
+    public static T Deserialize<T>(string filename, SerializerType serializerType)
     {
-      var serializer = new XmlSerializer(typeof(T));
-      try
-      {
-        using (var stream = new StreamReader(filename))
-          return (T)serializer.Deserialize(stream);
-      }
-      catch (Exception e)
-      {
-        throw new SerializationException("Can't deserialize an object of type " + typeof(T) + " from " + filename, e);
-      }
+      if (serializerType == SerializerType.Binary)
+        return DeserializeFromBinary<T>(filename);
+      if (serializerType == SerializerType.XML)
+        return DeserializeFromXml<T>(filename);
+      return default(T);
     }
 
     /// <summary>
-    /// Serializes the given data to the specified file as XML.
+    /// Serializes the given data to the specified file.
     /// </summary>
     /// <exception cref="SerializationException">
-    /// A <see cref="SerializationException"/> is thrown if the object's type can't be serialized to an XML-file.
+    /// A <see cref="SerializationException"/> is thrown if the object's type can't be serialized.
     /// </exception>
     /// <param name="filename">The file to serialize the data to.</param>
     /// <param name="data">The data to serialize.</param>
-    public static void Serialize(string filename, object data)
+    /// <param name="serializerType">The type of serializer to use.</param>
+    public static void Serialize(string filename, object data, SerializerType serializerType)
     {
-      var ns = new XmlSerializerNamespaces();
-      ns.Add("", "");
-      var serializer = new XmlSerializer(data.GetType());
-      try
-      {
-        using (var stream = new StreamWriter(filename))
-          serializer.Serialize(stream, data, ns);
-      }
-      catch (Exception e)
-      {
+      Exception e = null;
+      if (serializerType == SerializerType.Binary)
+        SerializeToBinary(filename, data, out e);
+      if (serializerType == SerializerType.XML)
+        SerializeToXml(filename, data, out e);
+      if (e != null)
         throw new SerializationException("Can't serialize an object of type " + data.GetType() + " to " + filename, e);
-      }
     }
 
     /// <summary>
-    /// Serializes the given data to the specified file as XML.
+    /// Serializes the given data to the specified file.
     /// </summary>
     /// <param name="filename">The file to serialize the data to.</param>
     /// <param name="data">The data to serialize.</param>
+    /// <param name="serializerType">The type of serializer to use.</param>
     /// <returns>True if serialization succeeded; False, otherwise.</returns>
-    public static bool TrySerialize(string filename, object data)
+    public static bool TrySerialize(string filename, object data, SerializerType serializerType)
     {
-      var serializer = new XmlSerializer(data.GetType());
-      try
-      {
-        using (var stream = new StreamWriter(filename))
-          serializer.Serialize(stream, data);
-        return true;
-      }
-      catch (Exception)
-      {
-        return false;
-      }
+      Exception e = null;
+      if (serializerType == SerializerType.Binary)
+        SerializeToBinary(filename, data, out e);
+      if (serializerType == SerializerType.XML)
+        SerializeToXml(filename, data, out e);
+      return e == null;
     }
 
     /// <summary>
@@ -114,7 +106,7 @@ namespace AppStract.Utilities.Helpers
     /// can't be deserialized to an object of type <see cref="T"/>.
     /// </exception>
     /// <returns></returns>
-    public static T DeserializeFromString<T>(string data)
+    public static T DeserializeFromXmlString<T>(string data)
     {
       var serializer = new XmlSerializer(typeof(T));
       try
@@ -137,7 +129,7 @@ namespace AppStract.Utilities.Helpers
     /// </exception>
     /// <param name="data">The data to serialize.</param>
     /// <returns></returns>
-    public static string SerializeToString(object data)
+    public static string SerializeToXmlString(object data)
     {
       var serializer = new XmlSerializer(data.GetType());
       try
@@ -156,6 +148,82 @@ namespace AppStract.Utilities.Helpers
           "Can't serialize an object of type " + data.GetType(), e);
       }
     }
+
+    #endregion
+
+    #region Private Methods - XML
+
+    private static T DeserializeFromXml<T>(string filename)
+    {
+      var serializer = new XmlSerializer(typeof(T));
+      try
+      {
+        using (var stream = new StreamReader(filename))
+          return (T)serializer.Deserialize(stream);
+      }
+      catch (Exception e)
+      {
+        throw new SerializationException("Can't deserialize an object of type " + typeof(T) + " from " + filename, e);
+      }
+    }
+
+    private static void SerializeToXml(string filename, object data, out Exception exception)
+    {
+      exception = null;
+      var ns = new XmlSerializerNamespaces();
+      ns.Add("", "");
+      var serializer = new XmlSerializer(data.GetType());
+      try
+      {
+        using (var stream = new StreamWriter(filename))
+          serializer.Serialize(stream, data, ns);
+      }
+      catch (Exception e)
+      {
+        exception = e;
+      }
+    }
+
+    #endregion
+
+    #region Private Methods - Binary
+
+    private static T DeserializeFromBinary<T>(string filename)
+    {
+      try
+      {
+        using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+        {
+          var formatter = new BinaryFormatter();
+          return (T) formatter.Deserialize(stream);
+        }
+      }
+      catch (Exception e)
+      {
+        throw new SerializationException("Can't deserialize an object of type " + typeof(T) + " from " + filename, e);
+      }
+    }
+
+    private static void SerializeToBinary(string filename, object data, out Exception exception)
+    {
+      exception = null;
+      try
+      {
+        if (File.Exists(filename))
+          File.Delete(filename);
+        using (var stream = new FileStream(filename, FileMode.Create, FileAccess.Write))
+        {
+          var formatter = new BinaryFormatter();
+          formatter.Serialize(stream, data);
+        }
+      }
+      catch (Exception e)
+      {
+        exception = e;
+      }
+    }
+
+    #endregion
 
   }
 }
