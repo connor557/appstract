beff023020bf4c31058c68a9038822af4034680b
simon_allaeys <simon_allaeys@hotmail.com>
2010-06-20 10:53:38 +0000

    Declared NativeAPI as private type of HostRegistry class.

--- AppStract/AppStract.Server/Registry/HostRegistry.cs
+++ AppStract/AppStract.Server/Registry/HostRegistry.cs
@@ -37,6 +37,103 @@ namespace AppStract.Server.Registry
   public static class HostRegistry
   {
 
+    #region Private Types
+
+    /// <summary>
+    /// Defines native registry API functions.
+    /// </summary>
+    private static class NativeAPI
+    {
+
+      /// <summary>
+      /// The <see cref="KeyInformationClass"/> enumeration type represents the type of information to supply about a registry key.
+      /// </summary>
+      /// <remarks>
+      /// Use the <see cref="KeyInformationClass"/> values to specify the type of data to be supplied by the ZwEnumerateKey and ZwQueryKey routines.
+      /// </remarks>
+      public enum KeyInformationClass
+      {
+        /// <summary>
+        /// A KEY_BASIC_INFORMATION structure is supplied.
+        /// </summary>
+        KeyBasicInformation = 0,
+        /// <summary>
+        /// A KEY_NODE_INFORMATION structure is supplied.
+        /// </summary>
+        KeyNodeInformation = 1,
+        /// <summary>
+        /// A KEY_FULL_INFORMATION structure is supplied.
+        /// </summary>
+        KeyFullInformation = 2,
+        /// <summary>
+        /// A KEY_NAME_INFORMATION structure is supplied.
+        /// </summary>
+        KeyNameInformation = 3,
+        /// <summary>
+        /// A KEY_CACHED_INFORMATION structure is supplied.
+        /// </summary>
+        KeyCachedInformation = 4,
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        KeyFlagsInformation = 5,
+        /// <summary>
+        /// A KEY_VIRTUALIZATION_INFORMATION structure is supplied.
+        /// </summary>
+        KeyVirtualizationInformation = 6,
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        KeyHandleTagsInformation = 7,
+        /// <summary>
+        /// The maximum value in this enumeration type.
+        /// </summary>
+        MaxKeyInfoClass = 8
+      }
+
+      /// <summary>
+      /// The ZwQueryKey routine provides information about the class of a registry key, and the number and sizes of its subkeys.
+      /// </summary>
+      /// <param name="KeyHandle">
+      /// Handle to the registry key to obtain information about. This handle is created by a successful call to ZwCreateKey or ZwOpenKey.
+      /// </param>
+      /// <param name="KeyInformationClass">
+      /// Specifies a KEY_INFORMATION_CLASS value that determines the type of information returned in the KeyInformation buffer.
+      /// </param>
+      /// <param name="KeyInformation">
+      /// Pointer to a caller-allocated buffer that receives the requested information.
+      /// </param>
+      /// <param name="bufferSize">
+      /// Specifies the size, in bytes, of the KeyInformation buffer.
+      /// </param>
+      /// <param name="resultSize">
+      /// Pointer to a variable that receives the size, in bytes, of the requested key information.
+      /// If ZwQueryKey returns STATUS_SUCCESS, the variable contains the amount of data returned.
+      /// If ZwQueryKey returns STATUS_BUFFER_OVERFLOW or STATUS_BUFFER_TOO_SMALL, you can use the value of the variable to determine the required buffer size.
+      /// </param>
+      /// <remarks>
+      /// The KeyHandle passed to ZwQueryKey must have been opened with KEY_QUERY_VALUE access.
+      /// </remarks>
+      /// <returns></returns>
+      [DllImport("ntdll.dll", SetLastError = true)]
+      public static extern NativeResultCode NtQueryKey(UIntPtr KeyHandle,
+                                                       KeyInformationClass KeyInformationClass,
+                                                       IntPtr KeyInformation,
+                                                       int bufferSize,
+                                                       out int resultSize);
+
+      /// <summary>
+      /// Closes a handle to the specified registry key.
+      /// </summary>
+      /// <param name="hKey">A handle to the open key to be closed.</param>
+      /// <returns></returns>
+      [DllImport("advapi32.dll", SetLastError = true)]
+      public static extern NativeResultCode RegCloseKey(UIntPtr hKey);
+
+    }
+
+    #endregion
+
     #region Public Methods
 
     /// <summary>
@@ -107,7 +204,7 @@ namespace AppStract.Server.Registry
     /// <returns></returns>
     public static bool CloseKey(uint hKey)
     {
-      return NativeAPI.RegCloseKey(hKey) == NativeResultCode.Success;
+      return NativeAPI.RegCloseKey(new UIntPtr(hKey)) == NativeResultCode.Success;
     }
 
     /// <summary>
