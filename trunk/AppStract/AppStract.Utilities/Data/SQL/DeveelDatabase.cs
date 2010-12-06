c511b863f7081fa8b330c78e158156ab5a966bd8
simon_allaeys@hotmail.com <simon_allaeys@hotmail.com@d5324d32-0291-11de-bcc6-d9108be23e13>
2010-12-06 16:02:13 +0000

    Fixed mixing of _connectionString and ConnectionString values

--- trunk/AppStract/AppStract.Utilities/Data/SQL/DeveelDatabase.cs
+++ trunk/AppStract/AppStract.Utilities/Data/SQL/DeveelDatabase.cs
@@ -36,13 +36,13 @@ namespace AppStract.Utilities.Data.Sql
 
     protected override void AssertConnectionString(string connectionString)
     {
-      if (!_connectionString.ToLowerInvariant().Contains("path="))
+      if (!connectionString.ToLowerInvariant().Contains("path="))
         throw new DatabaseException("The connectionstring must at least specify a data source: " + connectionString);
     }
 
-    protected override DbCommand CreateCommand(string connectionString, string command)
+    protected override DbCommand CreateCommand(string command)
     {
-      return new DeveelDbConnection(_connectionString).CreateCommand(command);
+      return new DeveelDbConnection(ConnectionString).CreateCommand(command);
     }
 
     protected override IDbDataParameter CreateParameter(string name, object value)
