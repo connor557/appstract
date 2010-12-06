20439648702dbaeda9da05e92960ca637f3f02c4
simon_allaeys <simon_allaeys@hotmail.com>
2010-12-06 15:26:13 +0000

    Added FileDatabase, DeveelDatabase and SqlCeDatabase classes

---
+++ AppStract/AppStract.Utilities/Data/SQL/SqlCeDatabase.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlServerCe;
+using System.IO;
+
+namespace AppStract.Utilities.Data.Sql
+{
+  public abstract class SqlCeDatabase<T> : SqlDatabase<T>
+  {
+  
+    #region Constants
+
+    /// <summary>
+    /// A format string for the default connectionstring.
+    /// Use like: <code>string.Format(DefaultConnectionStringFormat, myDataSourceVariable)</code>
+    /// </summary>
+    protected const string _DefaultConnectionStringFormat = "Data Source={0}";
+
+    #endregion
+
+    #region Constructors
+
+    protected SqlCeDatabase(string connectionString)
+      : base(connectionString)
+    {
+      
+    }
+
+    #endregion
+
+    #region Protected Methods
+
+    protected override void CreateDatabase()
+    {
+      var index = ConnectionString.IndexOf(';');
+      var filename = index > 0
+                       ? ConnectionString.Substring("Data Source=".Length, index)
+                       : ConnectionString.Substring("Data Source=".Length);
+      if (!File.Exists(filename))
+      {
+        var engine = new SqlCeEngine(ConnectionString);
+        engine.CreateDatabase();
+      }
+    }
+
+    protected static string GetDefaultConnectionString(string filename)
+    {
+      return string.Format(_DefaultConnectionStringFormat, filename);
+    }
+
+    protected override void AssertConnectionString(string connectionString)
+    {
+      if (!_connectionString.ToLowerInvariant().Contains("data source="))
+        throw new DatabaseException("The connectionstring must at least specify a data source: " + connectionString);
+    }
+
+    protected override DbCommand CreateCommand(string connectionString, string command)
+    {
+      return new SqlCeCommand(command, new SqlCeConnection(connectionString));
+    }
+
+    protected override IDbDataParameter CreateParameter(string name, object value)
+    {
+      return new SqlCeParameter(name, value);
+    }
+
+    #endregion
+
+  }
+}
